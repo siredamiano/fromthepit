97c45bbcffd66029e5a9c8011c6eda5cda83fc1f
Damiano <damian.a.torres@gmail.com>
2013-02-07 22:06:07 -0500

    Added more getters and setters and create_concert method to start

--- events.py
+++ events.py
@@ -8,7 +8,9 @@ class Event:
 		return self.date
 	
 	def set_date(self,date):
-		self.date = date	
+		self.date = date
+		
+	
 	
 class Concert(Event):
 	
@@ -22,8 +24,6 @@ class Concert(Event):
 	
 	def get_openers():
 		return self.openers
-		
-	
 	
 	def get_venue():
 		return self.venue
@@ -33,18 +33,52 @@ class Concert(Event):
 	
 	def set_openers(self,openers):
 		self.openers = openers	
-	
-	
 		
 	def set_venue(self, venue):
 		self.venue = venue
+		
+	def create_concert():
+		#TODO: HERE WE ARE GOING TO CREATE THE CONCERT OBJECT TO DB AND SAVE IT. ALSO CALL THE SUBSCRIPTION
+		pass
+		
 
 class Venue():
 	
-	def __init__(self, venue_name, country,city, state, info, location):
+	def __init__(self, venue_name, country,city, state, venue_info, location):
 		self.venue_name = venue_name
 		self.country = country
 		self.city = city
 		self.state = state
-		self.info = info
+		self.venue_info = venue_info
 		self.location = location
+			
+	def get_venue_name():
+		return self.venue_name
+
+	def get_country():
+		return self.country
+
+	def get_city():
+		return self.city	
+		
+	def get_state():
+		return self.state
+
+	def get_venue_info():
+		return self.venue_info
+
+	def get_location():
+		return self.location		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
